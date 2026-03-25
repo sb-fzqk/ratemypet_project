fc54e73fa9fe0e4684a057e243c89183c4a3ecc5
Lev Khyzhnychenko <3014673k@student.gla.ac.uk>
2026-03-25 14:07:27 +0000

    fixed the change password

--- ratemypet/views.py
+++ ratemypet/views.py
@@ -136,6 +136,14 @@ def edit_profile(request):
 
 @login_required
 def settings_views(request):
+    if request.method == "POST":
+        new_password = request.POST.get("new_password")
+
+        if new_password:
+            request.user.set_password(new_password)
+            request.user.save()
+
+            return redirect('login')
     return render(request, 'ratemypet/settings.html')
 
 @login_required
